20d04b08b1ee47954e456a56b98eabb9aa214364
quinncja <58446803+quinncja@users.noreply.github.com>
2023-10-10 20:29:50 -0700

    stripe endpoint

--- backend/api/order/Order.js
+++ backend/api/order/Order.js
@@ -1,51 +1,51 @@
-const mongoose = require("mongoose")
-const orderType = ["pickup", "delivery"]
+const mongoose = require("mongoose");
+const orderType = ["pickup", "delivery"];
 const orderSchema = new mongoose.Schema({
-    customerName: {
-        type: String,
-        required: true,
-    },
-    tip: {
-        type: Number,
-        required: true
-    },
-    totalPrice: {
-        type: Number,
-        required: true,
-    },
-    type: {
-        type: String,
-        required: true,
-        ENUM: orderType,
-    },
-    address: {
-        type: String,
-    },
-    notes: {
-        type: String,
-    },
-    utensils: {
-        type: Boolean,
-    },
-    phone: {
-        type: String,
-        required: true,
-    },
-    items: {
-        type: [Object],
-    },
-    timePlaced: {
-        type: Date,
-        default: Date.now
-    },  
-    estimatedReady: {
-        type: String,
-        default: "",
-    },
-    isPaid: {
-        type: Boolean,
-        default: false,
-    }
+  customerName: {
+    type: String,
+    required: true,
+  },
+  tip: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    ENUM: orderType,
+  },
+  address: {
+    type: String,
+  },
+  notes: {
+    type: String,
+  },
+  utensils: {
+    type: Boolean,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  items: {
+    type: [Object],
+  },
+  timePlaced: {
+    type: Date,
+    default: Date.now,
+  },
+  estimatedReady: {
+    type: String,
+    default: "",
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = mongoose.model("Order", orderSchema)
+module.exports = mongoose.model("Order", orderSchema);
