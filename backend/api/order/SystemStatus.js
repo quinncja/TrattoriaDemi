9045b41c548d5c0b1693bce866dd4b59ea8219ad
quinncja <58446803+quinncja@users.noreply.github.com>
2023-10-18 12:12:25 -0700

    SystemStatus obj

---
+++ backend/api/order/SystemStatus.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const systemStatusSchema = new mongoose.Schema({
+  pickup: {
+    type: Boolean,
+    default: true,
+  },
+  delivery: {
+    type: Boolean,
+    default: true,
+  }
+});
+
+const SystemStatus = mongoose.model('SystemStatus', systemStatusSchema);
+
+module.exports = SystemStatus;
