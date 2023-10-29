e5baf9ffe2f56fd0afdae0391531553964ecef48
quinncja <58446803+quinncja@users.noreply.github.com>
2023-10-29 16:27:20 -0700

    Better reserve on mobile

--- frontend/src/animations.js
+++ frontend/src/animations.js
@@ -11,3 +11,28 @@ export const fadeInDown = {
   exit: { opacity: 0, y: -20 },
   transition: { duration: 0.5 },
 };
+
+export const variants = {
+  hidden: { 
+    opacity: 0,
+    height: 0,
+    transition: {
+      when: 'afterChildren',
+      duration: 0.2
+    }
+  },
+  visible: {
+    opacity: 1,
+    height: 'auto',
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.05,
+      duration: 0.2
+    }
+  }
+};
+
+export const itemVariants = {
+  hidden: { opacity: 0, y: -10 },
+  visible: { opacity: 1, y: 0 }
+};
