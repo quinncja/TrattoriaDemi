4a3c1d9443d68886fc6a3f91f7f48d93cd443e76
quinncja <58446803+quinncja@users.noreply.github.com>
2023-11-06 20:34:34 -0800

    More changes?

--- frontend/src/svg.js
+++ frontend/src/svg.js
@@ -110,8 +110,6 @@ export const clockSvg = () => {
 export const trashCanSvg = () => {
   return (
     <svg
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
