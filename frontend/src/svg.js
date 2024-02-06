127487ece52d3c47ad716072cfaf70bd24500484
quinncja <58446803+quinncja@users.noreply.github.com>
2024-02-05 22:40:20 -0600

    New admin dashboard menu page

--- frontend/src/svg.js
+++ frontend/src/svg.js
@@ -326,3 +326,25 @@ export const leftArrow = () => {
     </svg>
   )
 }
+
+export const homeSvg = () => {
+  return(
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M14.78 21.25H9.22V15.3C9.22 14.75 9.67 14.3 10.22 14.3H13.78C14.33 14.3 14.78 14.75 14.78 15.3V21.25V21.25Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M21.25 11.39V18.1C21.25 19.84 19.91 21.25 18.25 21.25H5.75C4.09 21.25 2.75 19.84 2.75 18.1V11.39C2.75 10.45 3.14 9.57 3.83 8.97L10.08 3.48C11.19 2.5 12.81 2.5 13.92 3.48L20.17 8.97C20.86 9.57 21.25 10.45 21.25 11.39Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  )
+}
+
+export const menuSvg = () => {
+  return(
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 19.31H4.81C3.68 19.31 2.75 18.38 2.75 17.25V6.96997C2.75 5.83997 3.68 4.90997 4.81 4.90997H5.72" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 4.91999V19.31C11.69 18.81 10.77 17.48 8.98999 16.75C8.17999 16.42 7.41 16.31 6.81 16.28C6.26 16.26 5.84 15.8 5.84 15.26V3.87C5.84 3.29 6.31001 2.82003 6.89001 2.84003C7.46001 2.86003 8.14 2.94999 8.87 3.16999C10.4 3.61999 11.45 4.42999 12 4.91999Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M21.25 6.96997V17.25C21.25 18.38 20.32 19.31 19.19 19.31H12V4.91998H19.19C20.32 4.91998 21.25 5.83997 21.25 6.96997Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M15.07 7.96997H18.18" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M15.07 12.11H18.18" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M15.07 16.26H18.18" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  )
+}
