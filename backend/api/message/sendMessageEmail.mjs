9f12a70a4737f478f458388062c460463a17c321
quinncja <58446803+quinncja@users.noreply.github.com>
2024-07-04 00:32:17 -0500

    emails use env

--- backend/api/message/sendMessageEmail.mjs
+++ backend/api/message/sendMessageEmail.mjs
@@ -2,11 +2,12 @@ import { Resend } from "resend";
 import messageSent from "./messageSent.js";
 
 const resend = new Resend(process.env.RESEND_KEY);
+const sender = process.env.SENDER_EMAIL
 
 async function sendMessageEmail(message) {
     try {
         await resend.emails.send({
-          from: "Trattoria Demi <noreply@trattoriademi.com>",
+          from: sender,
           to: "trattoriademi@gmail.com",
           subject: "Contact Form Submission",
           react: messageSent({
