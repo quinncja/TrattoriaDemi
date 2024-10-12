0e729846a1119a5fe7ab483579acfeb4b3a73507
quinncja <quinnwsieja@gmail.com>
2024-10-12 16:44:32 -0500

    Inhouse prelim styling commit

--- frontend/src/pages/Dashboard/InHouse/DBInhouse.js
+++ frontend/src/pages/Dashboard/InHouse/DBInhouse.js
@@ -1,13 +1,10 @@
 import Reservations from "./Reservations";
-import Orders from "./Orders";
 import "./DBInhouse.css";
 
 function DBInhouse() {
   return (
     <div className="inhouse-container">
       <Reservations />
-      <div className="hr-db-line" />
-      <Orders />
     </div>
   );
 }
