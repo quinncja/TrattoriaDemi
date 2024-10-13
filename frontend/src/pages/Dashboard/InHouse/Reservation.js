46e3a82c3595cc073a56f56a499392b88e71258f
quinncja <quinnwsieja@gmail.com>
2024-10-13 00:20:35 -0500

    Removed box-shadow in favor of border + centered res icons

--- frontend/src/pages/Dashboard/InHouse/Reservation.js
+++ frontend/src/pages/Dashboard/InHouse/Reservation.js
@@ -19,7 +19,7 @@ export function Reservation(props) {
         <svg
           width="24"
           height="24"
-          viewBox="0 0 24 24"
+          viewBox="-.5 -2 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -47,7 +47,7 @@ export function Reservation(props) {
         <svg
           width="24"
           height="24"
-          viewBox="0 0 24 24"
+          viewBox="-.5 -2 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -81,7 +81,7 @@ export function Reservation(props) {
         <svg
           width="24"
           height="24"
-          viewBox="0 0 24 24"
+          viewBox="0 -3 24 26"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
